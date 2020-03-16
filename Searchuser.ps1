d18b3b2ee530e8a55be8db4e59896377cc66895d
SysAD-Fedorov <57136696+SysAD-Fedorov@users.noreply.github.com>
2020-03-16 20:22:46 +0300

    1
    1

---
+++ Searchuser.ps1
@@ -0,0 +1,38 @@
+﻿$objSearcher = New-Object System.DirectoryServices.DirectorySearcher
+$objSearcher.SearchRoot = "LDAP://OU=Users,OU=P16,OU=TOGS,DC=rosstat,DC=local"
+#$objSearcher.Filter = "(&(objectCategory=person)(!userAccountControl:1.2.840.113556.1.4.803:=2))"
+$users = $objSearcher.FindAll()
+# Количество учетных записей
+$users.Count 
+$users | ForEach-Object {
+   $user = $_.Properties
+   New-Object PsObject -Property @{
+	samaccountname = [string]$user.samaccountname
+	displayname = [string]$user.displayname 
+	sn = [string]$user.sn
+	givenname = [string]$user.givenname
+	initials = [string]$user.initials
+	userprincipalname = [string]$user.userprincipalname
+	description = [string]$user.description
+	telephonenumber = [string]$user.telephonenumber
+	mail = [string]$user.mail
+	ipphone = [string]$user.ipphone
+	physicaldeliveryofficename = [string]$user.physicaldeliveryofficename
+	department = [string]$user.department
+	extensionattribute10 = [string]$user.extensionattribute10
+    extensionattribute11 = [string]$user.extensionattribute11
+    extensionattribute12 = [string]$user.extensionattribute12
+	title = [string]$user.title
+	manager = [string]$user.manager
+	company = [string]$user.company
+	co = [string]$user.co
+	c = [string]$user.c
+	st = [string]$user.st
+	l = [string]$user.l
+	streetaddress = [string]$user.streetaddress
+	extensionattribute1 = [string]$user.extensionattribute1
+	extensionattribute2 = [string]$user.extensionattribute2
+	extensionattribute3 = [string]$user.extensionattribute3
+    
+   }
+} | Export-Csv -Delimiter ";" -Encoding utf8 -Path  "d:\_Работа\AD\Add+Change+search\All.csv"
---
+++ userchange.ps1
@@ -0,0 +1,43 @@
+﻿$users = Import-Csv "d:\_Работа\AD\Add+Change+search\All_.csv" -Delimiter ";"
+$props = $users | Get-Member -MemberType NoteProperty | Select -Expand name
+
+foreach ($user in $users)
+{
+	Try{
+	      $objuser = ([adsisearcher]"(userPrincipalName=$($user.userPrincipalName))").FindOne().GetDirectoryEntry()
+	}
+	
+	Catch {
+		continue
+	}
+    
+	$log = New-Object Text.StringBuilder
+	
+	$props | Foreach {
+            $prop = $_.ToLower()
+            $oldprop = $objuser.InvokeGet($prop)
+			if ($user.$prop -eq "")
+			{
+				$objuser.PutEx(1, $prop, 0)
+                $text = "{0} : old {1} new {2} prop {3} - {4}" -f $objuser.name[0],$oldprop,"не задано",$prop,(Get-Date)
+                $log.AppendLine($text) | Out-Null
+
+			} elseif ($user.$prop -cne $oldprop)
+			{
+				$objuser.Put("$prop",$user.$prop)
+                $text = "{0} : old {1} new {2} prop {3} - {4}" -f $objuser.name[0],$oldprop,$user.$prop,$prop,(Get-Date)
+                $log.AppendLine($text) | Out-Null
+            }
+      }
+    
+	Try {
+       $objuser.SetInfo()
+	   if($log.ToString())
+	   {
+	   		$log.ToString() | Out-File "d:\_Работа\AD\Add+Change+search\All_.txt" -Append
+	   }
+    }
+    Catch {
+        $_
+    }
+}
